ebb0cf6856acbe98324a166c969893f9f671d9c5
Hai Nguyen <Hai.NguyenTrung2@vn.bosch.com>
2016-10-20 11:23:31 +0700

    added code for Singleton Pattern demo

---
+++ singleton-pattern-demo/src/com/exam/singleton/SingleObject.java
@@ -0,0 +1,20 @@
+package com.exam.singleton;
+
+public class SingleObject {
+    private static SingleObject instance;
+
+    private SingleObject() {
+
+    }
+
+    public static SingleObject getInstance() {
+        if (instance == null) {
+            instance = new SingleObject();
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello world from singleton object");
+    }
+}
---
+++ singleton-pattern-demo/src/com/exam/singleton/SingletonDemo.java
@@ -0,0 +1,9 @@
+package com.exam.singleton;
+
+public class SingletonDemo {
+
+    public static void main(String[] args) {
+        SingleObject.getInstance().showMessage();
+    }
+
+}
